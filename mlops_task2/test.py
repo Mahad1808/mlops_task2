5766df17cbf090b0b1bb37f35fdcde24fb1f8ced
Mahad Rahat <95528513+Mahad-Rahat@users.noreply.github.com>
2024-06-14 19:33:38 +0500

    test cases updated

--- mlops_task2/test.py
+++ mlops_task2/test.py
@@ -1,13 +1,13 @@
 from main import mlops
-
+#testing that students don't exceed the limit
 mlopsObj = mlops(10)
 def test_getTotalStudents():
     assert mlopsObj.getTotalStudents() == 10
-
+#trying to add students
 def test_addStudents():
     mlopsObj.addStudents()
     assert mlopsObj.getTotalStudents() == 11
-
+#removal
 def test_removeStudents():
     mlopsObj.removeStudents()
     assert mlopsObj.getTotalStudents() == 10
